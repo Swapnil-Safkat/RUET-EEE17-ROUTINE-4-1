4b5357ae1eb44143238d6955ff70375e1b07d0ea
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-09-25 00:17:40 +0600

    headerdone

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const header = () => {
   return (
-    <div className='w-full shadow-lg py-5 flex justify-center items-center'>
-      <h1 className='text-accent text-2xl'>Ruet EEE 17</h1>
+    <div className='w-full shadow-lg py-4 flex flex-row justify-center items-center'>
+      <div className='border-2 border-slate-700 w-full'></div>
+      <h1 className='text-gray-200 font-["Cambria"] mx-10 w-1/3 tracking-wider font-bold text-2xl'>Ruet EEE 17</h1>
+      <div className='border-2 border-slate-700 w-full'></div>
     </div>
   );
 };
---
+++ src/Data/Teachers.js
@@ -0,0 +1,7 @@
+export const Teachers = [
+  {
+    name: 'aaa',
+    short: 'aaa',
+    img:''
+  },
+];
