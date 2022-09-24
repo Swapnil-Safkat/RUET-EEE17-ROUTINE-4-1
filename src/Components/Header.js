69ab6782ad4e04a82c4b8a0d23e4d503cb50090e
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-09-24 23:53:09 +0600

    ruet eee17 routine

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <div className='w-full shadow-lg py-5 flex justify-center items-center'>
+      <h1 className='text-accent text-2xl'>Ruet EEE 17</h1>
+    </div>
+  );
+};
+
+export default header;
